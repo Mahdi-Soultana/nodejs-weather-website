d945fae544d8b4235736689825257fad41f95ff7
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2020-08-01 18:56:15 -0700

    reinstall nodemon like dev in Package.json

--- src/utilis/weatherStack.js
+++ src/utilis/weatherStack.js
@@ -1,15 +1,15 @@
-const request = require("request")
+const request = require("request");
 const weatherStack = (longitude, altitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=148344f434109e4f9567f9470479cc7b&query=${altitude},${longitude}`;
     request({ url, json: true }, (err, res) => {
         if (err) {
-            callback(" unabled your Connection to coonnect in the Service Weather !", undefined)
+            callback(" unabled your Connection to coonnect in the Service Weather !", undefined);
         }
         else if (res.body.error) {
-            callback("unabled your location Try another Search!", undefined)
+            callback("unabled your location Try another Search!", undefined);
         } else {
            
-            callback(undefined,`${res.body.current.weather_descriptions[0]} . The Tempreture Here Is ${res.body.current.temperature} dergree, But it's Fell Like ${res.body.current.feelslike} dergree . and The Humudity there Is ${res.body.current.humidity}% . `)
+            callback(undefined,`${res.body.current.weather_descriptions[0]} . The Tempreture Here Is ${res.body.current.temperature} dergree, But it's Fell Like ${res.body.current.feelslike} dergree . and The Humudity there Is ${res.body.current.humidity}% . `);
         }
     })
 
